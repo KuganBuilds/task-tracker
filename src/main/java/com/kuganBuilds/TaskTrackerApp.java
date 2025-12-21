19bd29d05110e399e81ab9b2609508cef4f5fd38
kugan <kuganeeswaran25@gmail.com>
2025-12-21 18:00:46 +0530

    Initial project setuo with Maven and Java 21

---
+++ src/main/java/com/kuganBuilds/TaskTrackerApp.java
@@ -0,0 +1,10 @@
+package com.kuganBuilds;
+
+
+public class TaskTrackerApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Task Tracker Application Started");
+    }
+}
